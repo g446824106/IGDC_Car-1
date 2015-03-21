cb5734b2dd0ab5d0303b101bf252ad6e53062a14
IGDC <paris3@163.com>
2015-03-21 15:21:12 +0800

    Scene
    这个文件夹用于存储场景文件

---
+++ Assets/IGDC_Script/IGDC_NSWell/IGDC_NSWell_ConnectToSql.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IGDC_NSWell_ConnectToSql : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
